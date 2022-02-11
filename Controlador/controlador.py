cfcef0baa5184ff540e5d86943edc0dfa1ce1916
iYair <al046638@uacam.mx>
2022-02-11 00:08:11 -0600

    File Chooser funtion

--- Controlador/controlador.py
+++ Controlador/controlador.py
@@ -9,17 +9,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.openFileNameDialog)
 
-class App(QWidget):
 
-    def __init__(self):
-        super().__init__()
-        self.title = 'PyQt5 file dialogs - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
-        self.initUI()
+    # def __init__(self):
+    #     super().__init__()
+    #     self.title = 'PyQt5 file dialogs - pythonspot.com'
+    #     self.left = 10
+    #     self.top = 10
+    #     self.width = 640
+    #     self.height = 480
+    #     self.initUI()
     
     def initUI(self):
         self.setWindowTitle(self.title)
